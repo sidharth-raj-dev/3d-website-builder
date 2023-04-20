f83424217a5916ef9fa888f6687ecee539debc46
Sidharth Raj <sidharth-raj@outlook.com>
2023-04-20 20:35:20 +0530

    converted from asynchronous to synchronous

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { createMachine, interpret, assign } from 'xstate';
 
 const editorMachine = createMachine({
+    /** @xstate-layout N4IgpgJg5mDOIC5QFkCGBjAFgSwHZgAIA5VAWzADpsIAbMAYgHsAHMXA9R3AFze4PK4ArgG0ADAF1EoZo1jZu2LtJAAPRACYAHGIoBmAJxiAbBoCsAGhABPTTopmte5y9d6Avu6toseQiXIKTh4wVX5BIQIWNkh6dBo5QmDeMIE2UUkVWXlFZSQ1O11DE3MrWwRDPQoDAEZjeobG4w8vEB8cfGIySmTQ8PSo1nwIKloGbFJZACd+PQgBRggwGnEpfOyFJVwVdQQAdg0yxD0zKr2zT28MDv9uoK4U-uFBmJHqOnolul4COYWv1ZZOSbPKgXZ7AAsFA0Yj0NVKNk0ZmMDkubWufi6gTwm1QNGwAC88FBPlxKHgAG6MADW5NwuPxBLAAFpYOgYoD1sDctt8rs9BCDBQamIDHpzkcEDVDNCLq12piAnSGYTifQwFMpowphRmDRUNwAGba0hUemKPGEllsjmZLk5LY7Y6C4Wi8WWREIWoUc6eVq4RZwFQKzpKoEO0EFBDMjQQyXM4xokO3bFjcMg3lgxCnIViCHGLQSz3aGoUMRONxuJMY0N3XqpCIvYbpnlOhCxyV1AzV3y1wL1p6RaLDUZ0FuOvmIEVmYUGU7wj3lCHIns3LHKi2M4njyO7CGHT1aUtyq69lOUDVaqY7zNR05aH1HhHlIw+uWeIA */
     id: "Machine Name",
     initial: "initializing",
     states: {
@@ -37,58 +38,79 @@ const editorMachine = createMachine({
             },
         },
         initializing: {
-            entry: ["setScene", "setCamera", "setRenderer", "adjustRender", "startAnimationLoop"],
-            on: {
-                complete: {
-                    target: "idle",
-                },
+            invoke: {
+                src: "initializeScene",
+                id: "initialize-scene",
+                onDone: [
+                    {
+                        target: "idle",
+                        actions: ["setSceneCameraAndRenderer"]
+                    },
+                ],
+                onError: [
+                    {
+                        target: "error",
+                    },
+                ],
             },
+        },
+        error: {
+            entry: "setError"
         },
     },
     context: {
         scene: null,
         camera: null,
         renderer: null,
+        error: ''
     },
     predictableActionArguments: true,
     preserveActionOrder: true,
 }, {
     actions: {
-        setScene: assign((context) => {
+        setSceneCameraAndRenderer: assign((context, event) => {
             return {
                 ...context,
-                scene: new THREE.Scene()
+                ...event.data
             }
         }),
-        setCamera: assign((context) => {
+        setError: assign((context, event) => {
             return {
                 ...context,
-                camera: new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+                error: event.data
             }
-        }),
-        setRenderer: assign((context) => {
-            return {
-                ...context,
-                renderer: new THREE.WebGLRenderer()
-            }
-        }),
-        adjustRender: (context) => {
-            context.renderer.setSize(window.innerWidth, window.innerHeight);
-            document.body.appendChild(context.renderer.domElement);
-        },
-        startAnimationLoop: (context) => {
+        })
+    },
+    services: {
+        initializeScene: async (context, event) => {
+            const scene = new THREE.Scene();
+            const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+            const renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+
             function animate() {
                 requestAnimationFrame(animate);
-                context.renderer.render(context.scene, context.camera);
+                renderer.render(scene, camera);
             }
 
             animate();
+
+            return {
+                ...context,
+                scene,
+                camera,
+                renderer
+            };
+
+
         }
     }
 });
 
 const editorService = interpret(editorMachine).onTransition((state) => {
     console.log(state.value);
+    console.log(state.context);
 });
 
 editorService.start();
